b7060818dbdb16310af8753388d6bbb95b78efd0
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-09 06:36:23 +0000

    [add]routes作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,36 @@
 Rails.application.routes.draw do
+
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    resources :items, only: [:index, :show]
+    resource :customers, only: [:edit, :update] do
+      get "mypage" => "customers#mypage"
+      get "unsubscribe" => "customers#unsubscribe"
+      tatch "quit" => "customers#quit"
+    end
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+      collection do
+        delete "destroy_all" => "cart_items#destroy_all"
+      end
+    end
+    resource :orders, only: [:new, :create, :index, :show] do
+      post "confirm" => "orders#confirm"
+      get "thanks" => "orders#thanks"
+    end
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
+  end
+
   # 管理者用
   # URL /admin/sign_in
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
@@ -11,8 +43,8 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
-  
+
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
