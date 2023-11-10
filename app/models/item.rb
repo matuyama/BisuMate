66ded1215fb093f30bdafc778e69c7fb300845a7
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-10 21:14:16 +0000

    [add]customer機能追加完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+  belongs_to :genre
+  has_many :cart_items, dependent: :destroy
+  has_one_attached :item_image
+
+  def price_tax_including
+    (self.price_tax_excluded * 1.1).floor
+  end
+
 end
