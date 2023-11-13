5aa182636e1029ac57a2fe8fcfeaa2183bb29871
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-13 01:44:49 +0000

    [add]enum追加 ”

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,7 +6,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-
+    
   end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,6 @@ class Order < ApplicationRecord
   has_many :items, through: :order_details, dependent: :destroy
   has_many :customers, through: :addresses, dependent: :destroy
 
+  enum payment_method: { credit_card: 0, transfer: 1}
+
 end
