1724efd3183cbc42d4dfbf6ecef423004d90916b
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-13 21:51:01 +0000

    [add]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,13 @@
 class Public::AddressesController < ApplicationController
+  
+  def 
+    
+  end
+
+  private
+  
+  def address_params
+    params.require(:address).permit(:customer, :name, :postal_code, :address)
+  end
+
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
+  
+  belongs_to :customer
 
   def deliveryTarget
     "〒#{self.postal_code} #{self.address} #{self.name}"
