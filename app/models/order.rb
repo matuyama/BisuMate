4a508ae4121f8de56163f58ee4ec54e03a8dda7e
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-13 21:44:54 +0000

    [add]show追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,7 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1}
+  enum status:{ wait_for_payment: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
 
   def deliveryTarget
     "〒#{self.postal_code} #{self.address} \n#{self.name}"
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,4 +3,8 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
+  def subtotal
+    self.price_tax_included * amount
+  end
+
 end
