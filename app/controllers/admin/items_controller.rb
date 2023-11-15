a5e4e88f0242ba43e6902007b88c32ab3e6797f9
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-15 23:58:28 +0000

    [add]edit追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,6 +21,12 @@ class Admin::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  
 
   private
 
