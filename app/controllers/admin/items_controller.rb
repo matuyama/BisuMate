1c3084b33026b0be87f2a0a1ba6038d7eb944fa2
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-15 15:40:46 +0000

    [add]new追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,31 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_item_path(item)
+    else
+      flash[:notice] = "保存に失敗しました"
+      redirect_to new_admin_item_path
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
-  
+
   def item_params
     params.require(:item).permit(:genre_id, :name, :description, :size_sutra, :size_length, :price_tax_excluded, :is_on_sale)
   end
-  
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,28 @@
 class Item < ApplicationRecord
 
+    with_options presence: do
+    validates :genre_id
+    validates :name
+    validates :description
+    validates :size_sutra
+    validates :size_length
+    validates :price_tax_excluded
+    validates :is_on_sale
+  end
+
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_one_attached :item_image
 
   enum is_on_sale: { on_sale:true, sales_stop: false}
+
+  def get_item_image(width,height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
 
   def price_tax_including
     (self.price_tax_excluded * 1.1).floor
