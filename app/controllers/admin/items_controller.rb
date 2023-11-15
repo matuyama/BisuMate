8c008877fc03ab5a1dbeabb04f073cd891e9e210
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-15 01:51:03 +0000

    [add]index追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,13 @@
 class Admin::ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :description, :size_sutra, :size_length, :price_tax_excluded, :is_on_sale)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,8 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_one_attached :item_image
 
+  enum is_on_sale: { on_sale:true, sales_stop: false}
+
   def price_tax_including
     (self.price_tax_excluded * 1.1).floor
   end
