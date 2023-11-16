d7dbc8b76453330bd16e9ebd2b08c907300f56d7
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-16 23:50:10 +0000

    [add]update追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,11 @@
 class Admin::CustomersController < ApplicationController
+  
+  
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postail_code, :address, :phone_numbre, :email, :is_active)
+  end
+
 end
