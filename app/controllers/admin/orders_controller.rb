acca71858b3b41be233b1127118401bcb764f284
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-16 02:12:47 +0000

    [add]show追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,15 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+    @order.postage = 800
+    @total = 0
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :name, :billing_price, :payment_method, :postage, :postal_code, :address)
+  end
+
 end
