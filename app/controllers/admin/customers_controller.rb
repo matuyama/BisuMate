7248daea1b925d2428ed370d950e053c310fd913
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-17 01:31:46 +0000

    [add]show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,6 +7,12 @@ class Admin::CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  
 
   private
 
