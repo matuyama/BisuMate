3d46a5db77abcf429b49a7dcf7e0eb1cae497160
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-17 01:37:15 +0000

    [add]update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,12 +7,20 @@ class Admin::CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
-  
+
+  def update
+    customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      redirect_to admin_customer_path(customer)
+    else
+      flash[:notice] = "保存に失敗しました"
+      redirect_to admin_customer_path(customer)
+    end
+  end
 
   private
 
