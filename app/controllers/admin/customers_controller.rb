bb9c72c9f73203d11c7a3854abaeee2601750c55
matuyama <143941798+matuyama@users.noreply.github.com>
2023-11-17 00:57:13 +0000

    [add]show追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,10 +4,14 @@ class Admin::CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postail_code, :address, :phone_numbre, :email, :is_active)
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :address, :phone_numbre, :email, :is_active)
   end
 
 end
